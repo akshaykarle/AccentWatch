b4d05ab9959e835a5153b92b6ce961e2915f0e96
Nat Welch <nat@natwelch.com>
2014-02-06 14:25:37 -0800

    white space

--- AccentWatch/src/main/java/org/devcloud/accent/AudioRecordingActivity.java
+++ AccentWatch/src/main/java/org/devcloud/accent/AudioRecordingActivity.java
@@ -15,20 +15,23 @@ import android.widget.Toast;
 public class AudioRecordingActivity extends Activity {
   private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
   private static final String AUDIO_RECORDER_FILE_EXT_MP4 = ".mp4";
-  private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+  private static final String AUDIO_RECORDER_FOLDER = "AccentWatch";
 
   private MediaRecorder recorder = null;
   private int currentFormat = 0;
-  private int output_formats[] = { MediaRecorder.OutputFormat.MPEG_4,
-      MediaRecorder.OutputFormat.THREE_GPP };
-  private String file_exts[] = { AUDIO_RECORDER_FILE_EXT_MP4,
-      AUDIO_RECORDER_FILE_EXT_3GP };
+  private int output_formats[] = {
+    MediaRecorder.OutputFormat.MPEG_4,
+    MediaRecorder.OutputFormat.THREE_GPP
+  };
+  private String file_exts[] = {
+    AUDIO_RECORDER_FILE_EXT_MP4,
+    AUDIO_RECORDER_FILE_EXT_3GP
+  };
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-
     setButtonHandlers();
     enableButtons(false);
     setFormatButtonCaption();
@@ -51,9 +54,7 @@ public class AudioRecordingActivity extends Activity {
   }
 
   private void setFormatButtonCaption() {
-    ((Button) findViewById(R.id.btnFormat))
-        .setText(getString(R.string.audio_format) + " ("
-            + file_exts[currentFormat] + ")");
+    ((Button) findViewById(R.id.btnFormat)).setText(getString(R.string.audio_format) + " (" + file_exts[currentFormat] + ")");
   }
 
   private String getFilename() {
@@ -98,39 +99,17 @@ public class AudioRecordingActivity extends Activity {
     }
   }
 
-  private void displayFormatDialog() {
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    String formats[] = { "MPEG 4", "3GPP" };
-
-    builder.setTitle(getString(R.string.choose_format_title))
-        .setSingleChoiceItems(formats, currentFormat,
-            new DialogInterface.OnClickListener() {
-
-              @Override
-              public void onClick(DialogInterface dialog,
-                                  int which) {
-                currentFormat = which;
-                setFormatButtonCaption();
-
-                dialog.dismiss();
-              }
-            }).show();
-  }
-
   private MediaRecorder.OnErrorListener errorListener = new MediaRecorder.OnErrorListener() {
     @Override
     public void onError(MediaRecorder mr, int what, int extra) {
-      Toast.makeText(AudioRecordingActivity.this,
-          "Error: " + what + ", " + extra, Toast.LENGTH_SHORT).show();
+      Toast.makeText(AudioRecordingActivity.this, "Error: " + what + ", " + extra, Toast.LENGTH_SHORT).show();
     }
   };
 
   private MediaRecorder.OnInfoListener infoListener = new MediaRecorder.OnInfoListener() {
     @Override
     public void onInfo(MediaRecorder mr, int what, int extra) {
-      Toast.makeText(AudioRecordingActivity.this,
-          "Warning: " + what + ", " + extra, Toast.LENGTH_SHORT)
-          .show();
+      Toast.makeText(AudioRecordingActivity.this, "Warning: " + what + ", " + extra, Toast.LENGTH_SHORT).show();
     }
   };
 
@@ -138,28 +117,22 @@ public class AudioRecordingActivity extends Activity {
     @Override
     public void onClick(View v) {
       switch (v.getId()) {
-        case R.id.btnStart: {
-          Toast.makeText(AudioRecordingActivity.this, "Start Recording",
+        case R.id.btnStart:
+          Toast.makeText(
+              AudioRecordingActivity.this,
+              "Start Recording",
               Toast.LENGTH_SHORT).show();
-
           enableButtons(true);
           startRecording();
-
           break;
-        }
-        case R.id.btnStop: {
-          Toast.makeText(AudioRecordingActivity.this, "Stop Recording",
+        case R.id.btnStop:
+          Toast.makeText(
+              AudioRecordingActivity.this,
+              "Stop Recording",
               Toast.LENGTH_SHORT).show();
           enableButtons(false);
           stopRecording();
-
           break;
-        }
-        case R.id.btnFormat: {
-          displayFormatDialog();
-
-          break;
-        }
       }
     }
   };
